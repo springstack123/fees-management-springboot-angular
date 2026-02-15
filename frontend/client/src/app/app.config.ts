a4ce10622602da8c580e4c8bcf0bfa1a029f03bc
springstack123 <ankitagaikwad073@gmail.com>
2026-02-15 19:41:31 +0530

    Final working backend

--- frontend/client/src/app/app.config.ts
+++ frontend/client/src/app/app.config.ts
@@ -1,7 +1,10 @@
-import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-export const appConfig = {
-  providers: [
-    provideHttpClient()
-  ]
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideClientHydration(),provideHttpClient(withFetch())]
 };
--- frontend/client/src/main.ts
+++ frontend/client/src/main.ts
@@ -1,9 +1,6 @@
-import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient()
-  ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
