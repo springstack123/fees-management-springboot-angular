44bb8c9c03264c0e300736ba6e76bfadcc903841
springstack123 <ankitagaikwad073@gmail.com>
2026-02-15 17:43:09 +0530

    Fixed CSS budget issue

--- frontend/client/src/app/models/register.ts
+++ frontend/client/src/app/models/register.ts
@@ -1,14 +1,17 @@
 export class Register {
   id?: number;
+
   fullName!: string;
-  dob!: string;
+  dob!: string;          // MUST be string in Angular
   gender!: string;
+
   address!: string;
   phone!: string;
   emailid!: string;
   password!: string;
+
   course!: string;
   previousSchool!: string;
   qualification!: string;
-  percentage!: number;
+  percentage!: number;  // MUST be number
 }
